78cc24ad00ea2086ed29259d7024c66e3c546c20
fi-suffix <fadildafiamsyah@gmail.com>
2026-08-11 13:52:15 +0700

    styling dashboard

--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.dashboard')
+@section('title', 'Dashboard')
+@section('page-title', 'Users Overview')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-
-    @include('layouts.navigation')
-
+@section('content')
     @foreach ($alluser as $user)
         <h1>{{$user->name}}</h1>
         <h3>{{$user->email}}</h3>
     @endforeach
-</body>
 
-</html>
+@endsection
